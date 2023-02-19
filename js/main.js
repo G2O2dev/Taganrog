1e32ef1f3feb688a60926d58f8ccd1cf47d42144
G2O2dev <85354527+G2O2dev@users.noreply.github.com>
2023-02-19 15:59:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -106,10 +106,63 @@ function initMap() {
     map.controls.remove('rulerControl'); // удаляем контрол правил
 }
 
+function loadHrefSmoothScroll() {
+    function scrollIt(destination, duration = 1600, callback) {
+        const start = window.pageYOffset;
+        const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+        const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+        const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+        const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+        if ('requestAnimationFrame' in window === false) {
+            window.scroll(0, destinationOffsetToScroll);
+            if (callback) {
+                callback();
+            }
+            return;
+        }
+
+        function scroll() {
+            const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+            const t = Math.min(1, ((now - startTime) / duration));
+            const timeFunction = t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+            window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+            if (window.pageYOffset === destinationOffsetToScroll) {
+                if (callback) {
+                    callback();
+                }
+                return;
+            }
+
+            requestAnimationFrame(scroll);
+        }
+
+        scroll();
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(function (el) {
+        el.addEventListener("click", function () {
+            scrollIt(document.querySelector(this.getAttribute('href')));
+        });
+    });
+}
+
 loadAboutSlider();
 runTimeUpdater();
 runWeatherUpdater();
 loadFeedbackSlider();
-ymaps.ready(initMap);
+loadHrefSmoothScroll();
 
+
+let mapLoaded = false;
+let map = document.getElementById("ymap");
+window.addEventListener("scroll", () => {
+    if (!mapLoaded && map.getBoundingClientRect().top < window.innerHeight) {
+        mapLoaded = true;
+        ymaps.ready(initMap);
+    }
+});
 
